f92ce08893abab394215d1a31187154cdcb8e44b
cesiumos-build <73893871+cesiumos-build@users.noreply.github.com>
2020-11-08 10:36:10 +0530

    Update Android.mk

--- libhidl/Android.mk
+++ libhidl/Android.mk
@@ -44,3 +44,11 @@ LOCAL_INSTALLED_MODULE_STEM := android.hidl.manager@1.0.so
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include $(BUILD_SHARED_LIBRARY)
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libplatformconfig
+LOCAL_MODULE_TAGS := optional
+LOCAL_VENDOR_MODULE := true
+include $(BUILD_SHARED_LIBRARY)
